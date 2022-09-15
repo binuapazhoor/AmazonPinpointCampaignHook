c8fa879b2085f7f1cacd61566627e79c2126d8fc
binuapazhoor <58440253+binuapazhoor@users.noreply.github.com>
2022-09-15 08:44:28 -0500

    Update CampaignHookFunction.py

--- CampaignHookFunction.py
+++ CampaignHookFunction.py
@@ -13,6 +13,10 @@ def lambda_handler(event, context):
     for id,endpoint in endpoints.items():
         print(id)
         Attributes = endpoint['Attributes']
+        # Here is where you can add custom code to add dynamic attributes to an endpoint: 
+        # Call a webservice, look up a value from a database, 
+        # Call a CRM API to retrieve an offer for this endpoint. 
+        # You can also remove endpoints to filter them out of the campaign.
         Attributes["CampaignHookField1"] = newField
         print(Attributes)
         print(endpoint['Attributes'])
